4c70bb0d537416adcc8e155af9e7670d0e139d47
Heath Hughes <heath.hughes@daveramsey.com>
2025-01-25 08:27:04 -0600

    initial form

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import WebForm from './WebForm.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WebForm />
   </StrictMode>,
 )
