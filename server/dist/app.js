36f695a17df0a40bb68031aa0753b44d4ef5337d
Heath Hughes <heath.hughes@daveramsey.com>
2025-02-05 16:07:10 -0600

    added endpoint calls to load drop downs

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pg from 'pg';
+import cors from 'cors';
 import gamesRoutes from './routes/games.js';
 const { json } = bodyParser;
 const { Client } = pg;
 const app = express();
+app.use(cors());
 const port = 3000;
 app.use(json());
 app.use('/games', gamesRoutes);
